77bec1c12564c94a81974e42ee430a659ee4b7ac
Rjanna <156856641+Rjanna@users.noreply.github.com>
2024-04-06 09:54:11 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,43 +12,43 @@ namespace HotelManagementSystem
         static void Main(string[] args)
         {
             List<HotelRoom> ewankonaRooms = new List<HotelRoom>();
-            HotelRoom room1 = new HotelRoom(101, RoomStyle.Twin, 1500);
-            HotelRoom room2 = new HotelRoom(102, RoomStyle.King, 3000);
-            yananRooms.Add(room1);
-            yananRooms.Add(room2);
-            Hotel hotelYanan = new Hotel("Hotel Yanan", "123 GStreet, Takaw City", yananRooms);
+            Room room1 = new Room(101, RoomStyle.Twin, 1500);
+            Room room2 = new Room(102, RoomStyle.King, 3000);
+            ewankonaRooms.Add(room1);
+            ewankonaRooms.Add(room2);
+            Hotel hotelHakdog = new Hotel("Hotel Hakdog", "145 Iyak, Bangag City", ewankonaRooms);
 
-            List<HotelRoom> hotel456Rooms = new List<HotelRoom>();
-            HotelRoom hotel456Room1 = new HotelRoom(101, RoomStyle.Queen, 2000);
-            HotelRoom hotel456Room2 = new HotelRoom(102, RoomStyle.Queen, 2000);
-            hotel456Rooms.Add(hotel456Room1);
-            hotel456Rooms.Add(hotel456Room2);
-            Hotel hotel456 = new Hotel("Hotel 456", "Session Road, Baguio City", hotel456Rooms);
+            List<Room> itlogRooms = new List<Room>();
+            Room itlogRoom1 = new Room(101, RoomStyle.Queen, 2000);
+            Room itlogRoom2 = new Room(102, RoomStyle.Queen, 2000);
+            itlogRooms.Add(itlogRoom1);
+            itlogRooms.Add(itlogRoom2);
+            Hotel hotelItlog = new Hotel("Bangus Residence", "Session Road, Baguio City", itlogRooms);
 
-            HotelManagementSystem hms = new HotelManagementSystem();
-            hms.AddHotel(hotelYanan);
-            hms.AddHotel(hotel456);
+            HotelSystem hms = new HotelSystem();
+            hms.AddHotel(hotelHakdog);
+            hms.AddHotel(hotelItlog);
 
             hms.DisplayHotels();
 
-            hotelYanan.DisplayAvailableRooms();
+            hotelHakdog.DisplayAvailableRooms();
 
-            Guest terry = new Guest("Terry", "Addr 1", "terry@email.com", 63919129, "rawrhehe1");
-            hms.RegisterUser(terry);
+            Guest who = new Guest("Who", "SaKanal Street", "Whoknows@email.com", 63919129, "okay");
+            hms.RegisterUser(who);
 
-            hms.CreateBooking(hotelYanan, room1, terry, DateTime.Now, new DateTime(2024, 04, 16));
-            hotelYanan.DisplayBookedRooms();
-            terry.DisplayBookings();
+            hms.CreateBooking(hotelHakdog, room1, who, DateTime.Now, new DateTime(2024, 04, 16));
+            hotelHakdog.DisplayBookedRooms();
+            who.DisplayBookings();
 
-            Receptionist anna = new Receptionist("Anna", "Addr 2", "anna@email.com", 67890, "rawrhehe<3");
+            Receptionist anna = new Receptionist("Anna", "Addr 2", "anna@email.com", 67890, "boompanes");
             hms.RegisterUser(anna);
 
-            Booking res = new Booking(new DateTime(2024, 05, 01), new DateTime(2024, 05, 06), hotel456Room2);
-            anna.BookReservation(terry, res);
+            Booking res = new Booking(new DateTime(2024, 05, 01), new DateTime(2024, 05, 06), itlogRoom2);
+            anna.BookReservation(who, res);
 
-            terry.DisplayBookings();
+            who.DisplayBookings();
             hms.DisplayBookingDetails(1234567890);
-            Console.WriteLine(terry.TotalRoomCount);
+            Console.WriteLine(who.TotalRoomCount);
         }
     }
 }

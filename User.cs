755e9b817c8f485db63967c9ee1270b776540b74
Rjanna <156856641+Rjanna@users.noreply.github.com>
2024-04-06 09:57:14 +0800

    Update User.cs

--- User.cs
+++ User.cs
@@ -6,30 +6,24 @@ using System.Threading.Tasks;
 
 namespace HotelManagementSystem
 {
-    public class User
+     public class User
     {
-        private static int s_UserIDSeed = 6;
-        public int ID { get; set; }
+        private static int _idseed = 0;
         public string Name { get; set; }
         public string Address { get; set; }
         public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Password { get; set; }
-
-        public User(int ID, string name, string address, string email, string phoneNumber, string password)
+        public int PhoneNumber { get; set; }
+        public int ID { get; private set; }
+        protected string Password { get; set; }
+        public User(string name, string address, string email, int phoneNumber, string password)
         {
-            this.ID = s_UserIDSeed++;
             Name = name;
             Address = address;
             Email = email;
             PhoneNumber = phoneNumber;
+            ID = _idseed++;
             Password = password;
         }
 
-        public static User Register(string name, string address, string email, string phoneNumber, string password)
-        {
-            int userID = 1; 
-            return new User(userID, name, address, email, phoneNumber, password);
-        }
     }
 }
